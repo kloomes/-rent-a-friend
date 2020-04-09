287c28fe1604aae92017ac998d1907e0acf64e16
Karl Loomes <kloomes@hotmail.com>
2020-04-09 15:08:43 +0100

    User controller and show added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :friends, only: [:index, :show]
-  resources :user do
+  resources :users, only: [:show, :edit, :update] do
     resources :bookings, only: [:index, :new, :create]
   end
   resources :bookings, only: [:show, :edit, :update, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2020_04_09_111510) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
